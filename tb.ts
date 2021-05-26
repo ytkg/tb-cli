03bf7c0f7db9534ac7546e93ac6c0038ceac5457
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-05-27 01:03:19 +0900

    refactor

--- tb.ts
+++ tb.ts
@@ -1,15 +1,15 @@
 import { parse } from "https://deno.land/std@0.97.0/flags/mod.ts";
 import { VERSION } from "./version.ts";
 
-function main(args: string[]) {
-  const parsedArgs = parse(args);
+function main() {
+  const { _: args, ...options } = parse(Deno.args);
 
-  if (parsedArgs.version) {
+  if (options.version) {
     console.log(`tb-cli ${VERSION}`);
     Deno.exit(0);
   }
 }
 
 if (import.meta.main) {
-  main(Deno.args);
+  main();
 }
